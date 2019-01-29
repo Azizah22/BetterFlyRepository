18b733f61ee115ecc1f98a2fe8c6f0b2403b9273
Azizah <zezoazizah@gmail.com>
2019-01-29 22:41:50 +0300

    add some changes
    Signed-off-by: Azizah <zezoazizah@gmail.com>

--- app/src/main/java/com/example/betterfly/vHome.java
+++ app/src/main/java/com/example/betterfly/vHome.java
@@ -1,13 +1,30 @@
 package com.example.betterfly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class vHome extends AppCompatActivity {
+public class vHome extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_v_home);
+        setContentView(R.layout.activity_home);
+
+        TextView signOut= findViewById(R.id.signOut);
+        findViewById(R.id.signOut).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signOut:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
     }
 }
