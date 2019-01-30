39ab6b3f66943a77d38985cea2fd5032ad79dbf7
Azizah <zezoazizah@gmail.com>
2019-01-30 14:03:48 +0300

    Azizah
    Signed-off-by: Azizah <zezoazizah@gmail.com>

--- app/src/main/java/com/example/betterfly/MainActivity.java
+++ app/src/main/java/com/example/betterfly/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FirebaseApp.initializeApp(this);
 
         setContentView(R.layout.activity_main);
-
+        int azizah;
         mAuth = FirebaseAuth.getInstance();
 
         editTextEmail = findViewById(R.id.editTextEmail);
